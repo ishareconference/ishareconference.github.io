b38f9bac9314883d724d619164b23f99e0c9631b
Andrew Coelho <acoelho.baa@gmail.com>
2017-05-02 22:31:32 -0400

    Add CNAME

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,12 @@ var navDrawer = document.querySelector('.landing--drawer');
 var headerLogo = document.querySelector('.header--logo');
 var header = document.querySelector('.header--main');
 
-navButton.addEventListener('click', function(event) {
-  navButton.classList.toggle('active');
-  header.classList.toggle('dark');
-  header.classList.toggle('stuck');
-  navDrawer.classList.toggle('visible');
-  headerLogo.classList.toggle('visible');
-});
+if (navButton) {
+  navButton.addEventListener('click', function(event) {
+    navButton.classList.toggle('active');
+    header.classList.toggle('dark');
+    header.classList.toggle('stuck');
+    navDrawer.classList.toggle('visible');
+    headerLogo.classList.toggle('visible');
+  });
+}
